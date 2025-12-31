23d3158417c5c6f7dbe1b56df137bdf40313aa3e
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-31 03:30:40 -0800

    Update .sh

--- .sh
+++ .sh
@@ -5,15 +5,19 @@ flag() {
 		do [[ -e ".flags/$f" ]] || return 1
 	done
 }
+ts() {
+if flag local
+	then tsc "$@"
+	else npx tsc "$@"
+fi
+}
 rm -rf logs dist &> /dev/null || :
 then mkdir -p logs dist
 if flag local
 	then exec &> logs/main.log
+	else npm ci
 fi
-npx tsc
-if ! flag local
-	then npm ci
-fi
+ts
 while read -r f
 	do node dist/htmlp.js "$f" || :
 done < <(find src -name \*.htmlp)
